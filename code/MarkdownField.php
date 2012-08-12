f7917f7f61c92e89aa97437c1e9092777f1a1f19
Cam Spiers <camspiers@gmail.com>
2012-08-12 22:00:05 +1200

    UPDATE: Set field as changed

--- code/MarkdownField.php
+++ code/MarkdownField.php
@@ -6,7 +6,7 @@ class MarkdownField extends TextareaField
     function saveInto(DataObjectInterface $dataObject) {
         $fieldName = $this->name;
         if ($dataObject->$fieldName->hasMethod('setMarkdown')) {
-            $dataObject->$fieldName->setMarkdown($this->Value());
+            $dataObject->$fieldName->setMarkdown($this->Value(), true);
         }
     }
     
