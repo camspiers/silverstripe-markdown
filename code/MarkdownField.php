e163b5566faba802a69630e42fb93f25c9c2322d
Cam Spiers <camspiers@gmail.com>
2012-08-12 21:50:00 +1200

    FEATURE: Markdown field that saves into Markdown db type

--- code/MarkdownField.php
+++ code/MarkdownField.php
@@ -1,2 +1,13 @@
 <?php
 
+class MarkdownField extends TextareaField
+{
+
+    function saveInto(DataObjectInterface $dataObject) {
+        $fieldName = $this->name;
+        if ($dataObject->$fieldName->hasMethod('setMarkdown')) {
+            $dataObject->$fieldName->setMarkdown($this->Value());
+        }
+    }
+
+}
